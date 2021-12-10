f8a31a75522763d2e7c46f3c5f649930890e0f38
nforay <nforay@student.42.fr>
2021-12-10 17:00:54 +0100

    Added Day10

---
+++ 2021/day10/part2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+void	line_score(const std::string &str, std::vector<int> &scores)
+{
+	int					score = 0;
+	std::stack<char>	symbols;
+
+	for (auto &c : str) {
+		switch (c) {
+			case ')':
+				if (symbols.top() != '(')
+					return;
+				symbols.pop();
+				break;
+			case ']':
+				if (symbols.top() != '[')
+					return;
+				symbols.pop();
+				break;
+			case '}':
+				if (symbols.top() != '{')
+					return;
+				symbols.pop();
+				break;
+			case '>':
+				if (symbols.top() != '<')
+					return;
+				symbols.pop();
+				break;
+			default:
+				symbols.push(c);
+		}
+	}
+	while (symbols.size()) {
+		switch (symbols.top()) {
+			case '(':
+				score = score * 5 + 1;
+				symbols.pop();
+				break;
+			case '[':
+				score = score * 5 + 2;
+				symbols.pop();
+				break;
+			case '{':
+				score = score * 5 + 3;
+				symbols.pop();
+				break;
+			case '<':
+				score = score * 5 + 4;
+				symbols.pop();
+				break;
+			default:
+				std::cerr << "Unexpected symbol: " << symbols.top() << std::endl;
+				return;
+		}
+	}
+	scores.push_back(score);
+	return;
+}
+
+int main()
+{
+	std::string				line;
+	std::vector<int>		scores;
+
+	while (std::getline(std::cin, line))
+		line_score(line, scores);
+	sort(scores.begin(), scores.end());
+	std::cout << "Answer: " << scores[scores.size() / 2] << std::endl;
+	return (0);
+}
