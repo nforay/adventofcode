fc2504df76989b1c6de5bae7b479d51d82dec3d3
nforay <nforay@student.42.fr>
2021-12-13 17:34:20 +0100

    Added Day13

---
+++ 2021/day13/part2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <sstream>
+
+void fold_paper(std::vector<std::string> &paper, const std::pair<char, int> &fold) {
+	switch (fold.first) {
+		case 'x':
+			for (auto &line : paper) {
+				std::string::iterator it = line.begin();
+				for (int i = fold.second; i > 0; i--, it++) {
+					*it = (*it == '#' ? *it : line.back());
+					line.pop_back();
+				}
+				line.pop_back();
+			}
+			break;
+		case 'y':
+			std::vector<std::string>::iterator itvf = paper.begin();
+			while (paper.size() - 1 > fold.second) {
+				std::string::iterator itf = (*itvf).begin();
+				for (std::string::iterator itb = paper.back().begin(); itb != paper.back().end(); itb++, itf++) {
+					*itf = (*itf == '#' ? *itf : *itb);
+				}
+				paper.pop_back();
+				itvf++;
+			}
+			paper.pop_back();
+			break;
+	}
+}
+
+int main()
+{
+	std::list<std::pair<int, int>> 			dots;
+	std::list<std::pair<char, int>>			folds;
+	std::vector<std::string>				paper;
+	std::string								line;
+	int										width = 0;
+	int										height = 0;
+
+	while (std::getline(std::cin, line)) {
+		if (!line.size())
+			break;
+		std::stringstream	ss;
+		ss.str(line);
+		std::pair<int, int> dot;
+		ss >> dot.first; ss.ignore();
+		ss >> dot.second;
+		dots.push_back(dot);
+	}
+	while (std::getline(std::cin, line)) {
+		std::pair<char, int> fold;
+		fold.first = line[11];
+		fold.second = std::stoi(line.c_str() + 13);
+		folds.push_back(fold);
+	}
+	for (auto &dot : dots) {
+		width = std::max(width, dot.first);
+		height = std::max(height, dot.second);
+	}
+	paper.resize(height + 1, std::string(width + 1, '.'));
+	for (auto &dot : dots)
+		paper[dot.second][dot.first] = '#';
+	for (auto &fold : folds)
+		fold_paper(paper, fold);
+	std::cout << "The code is: " << std::endl;
+	for (auto &line : paper)
+		std::cerr << line << std::endl;
+	return (0);
+}
