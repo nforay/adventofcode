eb6b0fc8dffbeaeb4728e24203d26c3ba1ce909b
nforay <nforay@student.42.fr>
2021-12-19 17:17:59 +0100

    Added Day17

---
+++ 2021/day17/part2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <regex>
+
+typedef struct coord {
+	int x;
+	int y;
+} coord;
+
+bool ontarget(const coord &pos, const coord &start, const coord &end) {
+	return ((start.x <= pos.x && pos.x <= end.x) && (end.y <= pos.y && pos.y <= start.y));
+}
+
+bool launchprobe(coord vel, int &maxy, const coord &start, const coord &end)
+{
+	coord pos = {0, 0};
+	int height = 0;
+	while (!ontarget(pos, start, end) && pos.x <= end.x && pos.y >= end.y) {
+		pos.x += vel.x;
+		pos.y += vel.y;
+		height = std::max(height, pos.y);
+		vel.x = (vel.x < 0 ? vel.x + 1 : vel.x > 0 ? vel.x - 1 : vel.x);
+		vel.y--;
+	}
+	if (ontarget(pos, start, end))
+		maxy = std::max(maxy, height);
+	return (ontarget(pos, start, end));
+}
+
+int main()
+{
+	std::string		str;
+	coord			start, end;
+	std::regex		values_regex("^target area: x=(-?\\d+)..(-?\\d+), y=(-?\\d+)..(-?\\d+)$");
+	std::smatch		matches;
+	int				result = 0;
+	int				maxy = 0;
+
+	while (std::getline(std::cin, str)) {
+		if(std::regex_search(str, matches, values_regex)) {
+			start.x = stoi(matches[1].str());
+			end.x = stoi(matches[2].str());
+			start.y = stoi(matches[4].str());	// swap y so that min is top left corner
+			end.y = stoi(matches[3].str());		// max is bottom right corner
+			std::cerr << "min/max X: " << start.x << "," << end.x << " Y: " << start.y << "," << end.y << std::endl;
+			for (int x = 0; x <= end.x;  x++)
+				for (int y = end.y; y < std::max(abs(end.y), maxy); y++)
+					if (launchprobe({x, y}, maxy, start, end)) {
+						result++;
+						std::cerr << "Hit with velocity " << x << "," << y << std::endl;
+					}
+		}
+	}
+	std::cout << "Answer: " << result << std::endl;
+	return (0);
+}
